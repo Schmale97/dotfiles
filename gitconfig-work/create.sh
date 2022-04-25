31d822ef31d68ec17e1dc2aa3915b311679b4b07
Callum Smale <71267909+Schmale97@users.noreply.github.com>
2022-04-25 11:03:17 +0100

    generalise logic for creating seperate git profile for work

--- gitconfig-work/create.sh
+++ gitconfig-work/create.sh
@@ -21,35 +21,45 @@ confirm_input () {
     return 1;
 }
 
+CONFIG_FILE=$1
 EDIT_FILE="CREATE"
-if [[ -f ~/.gitconfig-morrisons ]];
+if [[ -f ~/$CONFIG_FILE ]];
 then
-    echo .gitconfig-morrisons already exists
+    echo $CONFIG_FILE already exists
     confirm_input "Would you like to overwrite?" && EDIT_FILE="UPDATE" || EDIT_FILE="NO"
 fi
 
-if [[ EDIT_FILE = "NO" ]]
+if [[ $EDIT_FILE = "NO" ]]
 then
-    echo leaving .gitconfig-morrisons unchanged
+    echo Leaving $CONFIG_FILE unchanged.
     exit 0
 fi
-if [[ EDIT_FILE = "CREATE" ]]
+if [[ $EDIT_FILE = "CREATE" ]]
 then
-    echo creating .gitconfig-morrisons
+    echo Creating $CONFIG_FILE...
 fi
-if [[ EDIT_FILE = "UPDATE" ]]
+if [[ $EDIT_FILE = "UPDATE" ]]
 then
-    echo updating .gitconfig-morrisons
+    echo Updating $CONFIG_FILE...
 fi
 
 LIMIT=10
 ITERABLE=1
 until [[ $ITERABLE -gt $LIMIT ]];
 do
-    read -p "Please enter your morrisons email address: " EMAIL
+    read -p "Please enter your work email address: " EMAIL
     confirm_input "Is this email correct?" && break || true
     ITERABLE=$((ITERABLE+1))
 done
-touch ~/.gitconfig-morrisons
-cat ./gitconfig-morrisons/template > ~/.gitconfig-morrisons 
-sed -i "s/:::EMAIL:::/$EMAIL/g" ~/.gitconfig-morrisons 
+touch ~/$CONFIG_FILE
+cat ./gitconfig-work/template-work-profile > ~/$CONFIG_FILE 
+sed -i "s/<<Email>>/$EMAIL/g" ~/$CONFIG_FILE
+
+if [[ $EDIT_FILE = "CREATE" ]]
+then
+    echo Created $CONFIG_FILE.
+fi
+if [[ $EDIT_FILE = "UPDATE" ]]
+then
+    echo Updated $CONFIG_FILE.
+fi
