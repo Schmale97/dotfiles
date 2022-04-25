aa6286b3b3c6e099b3ef8b8ef6305c6d48ad6ee5
Callum Smale <71267909+Schmale97@users.noreply.github.com>
2022-04-25 10:33:55 +0100

    improve script for setting work config

--- gitconfig-morrisons/create.sh
+++ gitconfig-morrisons/create.sh
@@ -1,3 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-[ ! -f ~/.gitconfig ] && (touch ~/.gitconfig-morrisons && cat template > ~/.gitconfig-morrisons) || echo '.gitconfig-morrisons already exists'
+confirm_input () {
+    LIMIT=10
+    ITERABLE=1
+
+    until [[ $ITERABLE -gt $LIMIT ]];
+    do
+        [[ $ITERABLE -eq 1 ]] && ATTEMPT="" || ATTEMPT="[$ITERABLE/$LIMIT] - "
+        read -p "$ATTEMPT$1 (Y/N): " CONFIRM
+        if [[ $CONFIRM == [yY] || $CONFIRM == [yY][eE][sS] ]];
+        then
+            return 0;
+        fi
+        if [[ $CONFIRM == [nN] || $CONFIRM == [nN][oO] ]];
+        then
+            return 1;
+        fi
+        ITERABLE=$((ITERABLE+1))
+    done
+    return 1;
+}
+
+EDIT_FILE="CREATE"
+if [[ -f ~/.gitconfig-morrisons ]];
+then
+    echo .gitconfig-morrisons already exists
+    confirm_input "Would you like to overwrite?" && EDIT_FILE="UPDATE" || EDIT_FILE="NO"
+fi
+
+if [[ EDIT_FILE = "NO" ]]
+then
+    echo leaving .gitconfig-morrisons unchanged
+    exit 0
+fi
+if [[ EDIT_FILE = "CREATE" ]]
+then
+    echo creating .gitconfig-morrisons
+fi
+if [[ EDIT_FILE = "UPDATE" ]]
+then
+    echo updating .gitconfig-morrisons
+fi
+
+LIMIT=10
+ITERABLE=1
+until [[ $ITERABLE -gt $LIMIT ]];
+do
+    read -p "Please enter your morrisons email address: " EMAIL
+    confirm_input "Is this email correct?" && break || true
+    ITERABLE=$((ITERABLE+1))
+done
+touch ~/.gitconfig-morrisons
+cat ./gitconfig-morrisons/template > ~/.gitconfig-morrisons 
+sed -i "s/:::EMAIL:::/$EMAIL/g" ~/.gitconfig-morrisons 
